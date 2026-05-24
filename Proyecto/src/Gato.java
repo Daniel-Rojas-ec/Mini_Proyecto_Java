0c78c88ab687c0f83aceaef384cf09003b389cd0
Daniel Rojas <alejandro.r.u.ec@gmail.com>
2026-05-24 13:22:01 -0500

    Proyecto Java-Creamos las herencias y el contructo de las subclases de perro y gato

--- Proyecto/src/Gato.java
+++ Proyecto/src/Gato.java
@@ -1,3 +1,13 @@
-public class Gato {
+public class Gato extends Animal
+{
+    public Gato(int id, String nombre, int edad, String estado)
+    {
+        super(id, nombre, edad, estado);
+    }
 
+    @Override
+    public String hacerSonido()
+    {
+        return "Miau miau miau";
+    }
 }
--- Proyecto/src/Perro.java
+++ Proyecto/src/Perro.java
@@ -1,3 +1,13 @@
-public class Perro {
+public class Perro extends Animal
+{
+    public Perro(int id, String nombre, int edad, String estado)
+    {
+        super(id, nombre, edad, estado);
+    }
 
+    @Override
+    public String hacerSonido()
+    {
+        return "Guau guau guau";
+    }
 }
